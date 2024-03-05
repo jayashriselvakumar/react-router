ee213978b345907bdbc09f9812c5194afa3744f3
Jayashri selvakumar <jayashriselvakumar@gmail.com>
2024-03-05 17:28:32 +0530

    React lazy loading router

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './Components/Home';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import OrderSummary from './Components/OrderSummary';
 import NoMatch from './Components/NoMatch';
 import Products from './Components/Products';
 import FeaturedProducts from './Components/FeaturedProducts';
 import NewProducts from './Components/NewProducts';
+import React from 'react';
+const LazyAbout = React.lazy(() => import('./Components/About'))
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='order-summary' element={<OrderSummary/>}/>
-      <Route path="products" element={<Products/>}>
-        <Route index element={<FeaturedProducts/>}/>
-        <Route path="featured" element={<FeaturedProducts/>}/>
-        <Route path="new" element={<NewProducts/>}/>
-      </Route>
-      <Route path='*' element={<NoMatch/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={
+          <React.Suspense fallback='Loading...'>
+            <LazyAbout />
+          </React.Suspense>
+        } />
+        <Route path='order-summary' element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path='*' element={<NoMatch />} />
+      </Routes>
     </>
   );
 }
