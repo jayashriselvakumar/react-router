839e7868634743ffe1b6458b366e2a364d8aa756
Jayashri selvakumar <jayashriselvakumar@gmail.com>
2024-03-05 16:59:36 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+import OrderSummary from './Components/OrderSummary';
+import NoMatch from './Components/NoMatch';
+import Products from './Components/Products';
+import FeaturedProducts from './Components/FeaturedProducts';
+import NewProducts from './Components/NewProducts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='order-summary' element={<OrderSummary/>}/>
+      <Route path="products" element={<Products/>}>
+        <Route index element={<FeaturedProducts/>}/>
+        <Route path="featured" element={<FeaturedProducts/>}/>
+        <Route path="new" element={<NewProducts/>}/>
+      </Route>
+      <Route path='*' element={<NoMatch/>}/>
+    </Routes>
+    </>
   );
 }
 
